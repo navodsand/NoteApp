abe88af65b90cfb2662d0091e1af8670c51797eb
navodsand <sandaruwannavod223@gmail.com>
2023-06-22 20:08:12 +0530

    NoteApp

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-var form = document.getElementById("form");
-var ntitle = document.getElementById("note-title");
-var nbody = document.getElementById("note-body");
+let form = document.getElementById("form");
+let ntitle = document.getElementById("note-title");
+let nbody = document.getElementById("note-body");
 
 
 form.addEventListener('submit', addNote);
@@ -15,13 +15,22 @@ function addNote(e){
 
     else{
 
-        var tr = document.createElement('tr');
+        let tr = document.createElement('tr');
         tr.className = 'item';
 
-        var td1 = document.createElement('td');
+        let td1 = document.createElement('td');
         td1.appendChild(document.createTextNode(note-title.value));
-        var span = document.createElement('span');
-        span.className = 'note-body'
+        let span = document.createElement('span');
+        span.className = 'note-body';
+        span.appendChild(document.createTextNode(nbody.value));
+        td1.appendChild(span);
+
+        let td2 = document.createElement('td');
+        td2.className = 'btcellv';
+        let btn1 = document.createElement('button');
+        btn1.appendChild(document.createTextNode('view'));
+        btn1.setAttribute('id','vw');
+        td2.appendChild(btn1);
     }
 
 }
